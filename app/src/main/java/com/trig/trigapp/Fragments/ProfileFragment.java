18294ecf9b26468970bcec585fedccc1c4d9ded7
tushar <tusharpandey1993@gmail.com>
2020-09-17 23:36:46 +0530

    Added custom views

---
+++ app/src/main/java/com/trig/trigapp/Fragments/ProfileFragment.java
@@ -0,0 +1,51 @@
+package com.trig.trigapp.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.activity.OnBackPressedCallback;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.Navigation;
+import com.trig.trigapp.R;
+
+public class ProfileFragment extends Fragment {
+
+    private static final String TAG = "ProfileFragment";
+    FragmentActivity mActivity;
+    View mView;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mActivity = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.profile, container, false);
+        init(mView);
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                Navigation.findNavController(requireActivity(),R.id.navHostFragment)
+                        .navigate(R.id.action_fragment_profile_to_dashboardFragment);
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
+        return mView;
+    }
+
+    private void init(View mView) {
+    }
+
+}
