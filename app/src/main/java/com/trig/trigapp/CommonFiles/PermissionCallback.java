8468cb530df3b9041f7ca92b216ce73ec340212c
tushar <tusharpandey1993@gmail.com>
2020-09-22 22:33:55 +0530

    Added back buttons navigation

---
+++ app/src/main/java/com/trig/trigapp/CommonFiles/PermissionCallback.java
@@ -0,0 +1,11 @@
+package com.trig.trigapp.CommonFiles;
+
+/**
+ * Created by prasma04 on 15-06-2018.
+ */
+
+public interface PermissionCallback {
+    void permissionGranted();
+
+    void permissionRefused();
+}
